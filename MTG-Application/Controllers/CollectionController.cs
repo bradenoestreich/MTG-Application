db39c78bbe4cda300fe0ccca0f3a9a70c513d169
Braden Oestreich <braden.i.oestreich@gmail.com>
2022-06-20 13:52:08 -0400

    added routing and structure for collection view

---
+++ MTG-Application/Controllers/CollectionController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTG_Application.Data;
+using MTG_Application.Models;
+using MTG_Application.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MTG_Application.Controllers
+{
+    public class CollectionController : Controller
+    {
+        private CardDbContext context;
+
+        public CollectionController(CardDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Card> cards = new List<Card>(context.Cards.ToList());
+            return View(cards);
+        }
+
+        public IActionResult Add()
+        {
+            AddCardViewModel addCardViewModel = new AddCardViewModel();
+
+            return View(addCardViewModel);
+        }
+    }
+}
---
+++ MTG-Application/ViewModels/AddCardViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MTG_Application.ViewModels
+{
+    public class AddCardViewModel
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
